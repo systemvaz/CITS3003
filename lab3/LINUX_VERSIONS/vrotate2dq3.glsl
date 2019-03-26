204c66d8627e11a7d54db492edfc85609e848fdd
systemvaz <systemvaz@outlook.com>
2019-03-26 16:56:52 +0800

    update

--- lab3/LINUX_VERSIONS/vrotate2dq3.glsl
+++ lab3/LINUX_VERSIONS/vrotate2dq3.glsl
@@ -15,9 +15,9 @@ main()
 {
     gl_Position = vec4(vPosition.x * multipliers[0][0] + vPosition.y * multipliers[0][1] + vPosition.z * multipliers[0][2],
                    vPosition.x * multipliers[1][0] + vPosition.y * multipliers[1][1] + vPosition.z * multipliers[1][2],
-                   vPosition.x * multipliers[2][0] + vPosition.y * multipliers[2][1] + vPosition.z * multipliers[2][2],
+                   vPosition.x * multipliers[2][0] + vPosition.y * multipliers[2][1] + ((vPosition.z * multipliers[2][2])+1),
                    1.0);
 
-    mycolor = vec4(gl_Position.x, gl_Position.y, gl_Position.z, 1.0 );
+    mycolor = vec4(gl_Position.x, gl_Position.y, gl_Position.z - 0.2, 1.0 );
 
 }
